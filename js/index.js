9796a9705c5e2913fbf0d83baa9154ac06aca64c
Buvaneshvaran <52979995+MrX1068@users.noreply.github.com>
2020-10-12 03:41:54 +0530

    nearly completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-<!-- java script -->
+
+// var customClickColor = document.getElementById("test");
+
+// customClickColor.onClick = function() {
+//     customClickColor.style.background = "#5fa6d7"
+// }
+
+// alert("hai")
+
+var down = document.getElementById("download");
+
+down.onclick = function () {
+    alert("innum add panala aprm try pannavum...yours faithfully  :) ");
+}
+
+var github = document.getElementById("github");
+
+github.onmouseover = function () {
+    alert("My GitHub https://MrX1068.github.com/");
+}

2cb0b522f656449ae7534f454d7c3d064124359e
Buvaneshvaran <52979995+MrX1068@users.noreply.github.com>
2020-10-13 04:23:17 +0530

    end up asap...

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 
-// var customClickColor = document.getElementById("test");
+//  var customClickColor = document.getElementById("linux");
 
-// customClickColor.onClick = function() {
-//     customClickColor.style.background = "#5fa6d7"
-// }
+//  customClickColor.onclick = function() {
+//      customClickColor.style.background = "#5fa6d7"
+//  }
 
-// alert("hai")
 
 var down = document.getElementById("download");
 
@@ -13,8 +12,53 @@ down.onclick = function () {
     alert("innum add panala aprm try pannavum...yours faithfully  :) ");
 }
 
+/* mail link */
+
+// var mail = document.getElementById("mail");
+
+// mail.onclick = function () {
+//     window.location.href = "mailto:buvaneshvaran1068@gmail.com" ;
+// }
+
+/* githublink */
 var github = document.getElementById("github");
 
-github.onmouseover = function () {
-    alert("My GitHub https://MrX1068.github.com/");
+github.onclick = function () {
+    window.location.href = "https://github.com/MrX1068";
 }
+
+/* linkedin link */
+
+var linkedin = document.getElementById("linkedin");
+
+linkedin.onclick = function () {
+    window.location.href = "https://in.linkedin.com/in/mrx1068";
+}
+
+/* telegram link */
+
+var telegram = document.getElementById("telegram");
+
+telegram.onclick = function () {
+    window.location.href = "https://t.me/mrx1068";
+}
+
+/* wapplink */
+// var wapp = document.getElementById("wapp");
+
+// wapp.onclick = function () {
+//     window.location.href = "https://wa.me/917358891043" ;
+// }
+
+/* instalink */
+// var insta = document.getElementById("insta");
+
+// insta.onclick = function () {
+//     window.location.href = "" ;
+// }
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+

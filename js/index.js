0ef0bfa2a1a3525876db86af60d510a494bd1c96
Buvaneshvaran <52979995+MrX1068@users.noreply.github.com>
2020-10-23 01:16:56 +0530

    final update, everything completed.

--- js/index.js
+++ js/index.js
@@ -64,3 +64,16 @@
 // $(document).ready(function () {
 //     new WOW().init();
 // });
+
+$(window).on('load', function() {
+  $('#mdb-preloader').delay(1000).fadeOut(300);
+});
+
+$( document ).ready(function() {
+  new WOW().init();
+});
+
+
+
+
+
